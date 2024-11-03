ab351e46717d08b95160dba7490ffe8e34a877c3
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-11-03 14:22:32 +0300

    Created: Separate footer component

---
+++ src/app/footer.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer className="row-start-3 w-full flex gap-6 opacity-50 py-5 flex-wrap items-center justify-center">
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://www.linkedin.com/in/negasi-haile-abadi/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          aria-hidden
+          src="/file.svg"
+          alt="File icon"
+          width={16}
+          height={16}
+        />
+        LinkedIn →
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://github.com/NegasiHaile"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          aria-hidden
+          src="/window.svg"
+          alt="Window icon"
+          width={16}
+          height={16}
+        />
+        GitHub →
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://www.upwork.com/freelancers/~01556d2b924254de6d"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          aria-hidden
+          src="/globe.svg"
+          alt="Globe icon"
+          width={16}
+          height={16}
+        />
+        Upwork →
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,12 @@
-import Image from "next/image";
-
 import Main from "./main";
+import Footer from "./footer";
 
 export default function Home() {
   return (
-    <div className="w-full sm:w-[90%] md:w-[85] lg:w-[65%] font-[family-name:var(--font-geist-sans)]">
+    <div className="w-full sm:w-[90%] md:w-[85] lg:w-[60%] font-[family-name:var(--font-geist-sans)]">
       <Main />
 
-      <footer className="row-start-3 w-full flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          LinkedIn →
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          GitHub →
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Upwork →
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }

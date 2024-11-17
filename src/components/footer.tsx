7c0ce0beb7413cb3a49e1f34a5aae42b3bc577da
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-11-18 02:59:06 +0300

    Updated: Footer with the social accounts

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,68 +1,26 @@
+import { SocialAccountsTypes } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
+
+import { social_accounts } from "@/data/social_accounts";
 
 const Footer = () => {
   return (
-    <footer className="row-start-3 w-full flex gap-6 opacity-50 py-12 flex-wrap items-center justify-center">
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://www.linkedin.com/in/negasi-haile-abadi/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="/file.svg"
-          alt="File icon"
-          width={16}
-          height={16}
-        />
-        LinkedIn →
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://www.linkedin.com/in/negasi-haile-abadi/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="/file.svg"
-          alt="File icon"
-          width={16}
-          height={16}
-        />
-        Google Scholar →
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://github.com/NegasiHaile"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="/window.svg"
-          alt="Window icon"
-          width={16}
-          height={16}
-        />
-        GitHub →
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://www.upwork.com/freelancers/~01556d2b924254de6d"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="/globe.svg"
-          alt="Globe icon"
-          width={16}
-          height={16}
-        />
-        Upwork →
-      </a>
+    <footer className="row-start-3 w-full flex gap-6 opacity-50 py-12 flex-wrap items-center justify-between px-2">
+      {social_accounts.map((account, i) => {
+        return (
+          <Link
+            key={i}
+            className="flex items-center gap-1 hover:underline hover:underline-offset-4"
+            href={account.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {account.icon}
+            <span className="hidden md:block">→ {account.title}</span>
+          </Link>
+        );
+      })}
     </footer>
   );
 };

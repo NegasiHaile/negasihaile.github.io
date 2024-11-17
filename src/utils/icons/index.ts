1e6cab601dd2888bd5efc440d2509376c917cd26
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-11-18 02:57:40 +0300

    Added: Social account icons

--- src/utils/icons/index.ts
+++ src/utils/icons/index.ts
@@ -2,5 +2,22 @@ import RightArrow from "./RightArrow";
 import IconArrowRipple from "./IconArrowRipple";
 import IconStart from "./IconStart";
 import IconExpand from "./IconExpand";
+import IconLinkedIn from "./IconLinkedIn";
+import IconGitHub from "./IconGitHub";
+import IconYoutube from "./IconYoutube";
+import IconX from "./IconX";
+import IconUpwork from "./IconUpwork";
+import IconGoogleScholar from "./IconGoogleScholar";
 
-export { IconArrowRipple, IconExpand, RightArrow, IconStart };
+export {
+  IconArrowRipple,
+  IconExpand,
+  IconGitHub,
+  IconGoogleScholar,
+  IconLinkedIn,
+  RightArrow,
+  IconStart,
+  IconUpwork,
+  IconX,
+  IconYoutube,
+};

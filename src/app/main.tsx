b44610cc6da9076a607e141fe7189e47a8fdac0b
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-11-17 19:23:08 +0300

    Added: Turbo for development performance

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Main = () => {
@@ -50,12 +51,15 @@ const Main = () => {
     },
   ];
   return (
-    <main className="w-full px-5 py-2">
+    <main className="w-full space-y-12">
       {/* OVERVIEW */}
       <div className="border rounded-md p-5 pb-10 space-y-5">
         {/* WELCOME SECTION */}
-        <div className="py-2 border-b w-full">
+        <div className="flex flex-col md:flex-row md:justify-between items-center py-2 border-b w-full">
           <p className="font-bold text-2xl">Negasi Haile</p>
+          <p className="text-sm opacity-85 text-center">
+            Full-stck Dev'| Digital Healthcare Solutions | Health AI | NLP
+          </p>
         </div>
 
         {/* PROFILE SECTIONS*/}
@@ -65,7 +69,7 @@ const Main = () => {
             healthcare and improve patient outcomes.
           </p>
 
-          <ul className="list-disc space-y-2 pl-10">
+          <ul key="my-works-list" className="list-disc space-y-2 pl-10">
             {list.map((list, i) => {
               return (
                 <>
@@ -74,7 +78,7 @@ const Main = () => {
                     <ul key={`${i}_sub`} className="list-disc space-y-2 pl-10">
                       {list?.sublists.map((sublist, si) => {
                         return (
-                          <li key={si + `_` + sublist.title}>
+                          <li key={si + `_mini_sub_` + sublist.title}>
                             {sublist.description}
                           </li>
                         );
@@ -85,6 +89,26 @@ const Main = () => {
               );
             })}
           </ul>
+        </div>
+      </div>
+      {/* PROJECTS SECTIONS */}
+      <div className="w-full space-y-2">
+        <div className="flex justify-between items-center">
+          <p>Pinned Projects</p>
+          <Link
+            href={"/"}
+            className="text-blue-500 capitalize"
+            title="See all projects"
+          >
+            See all projects
+          </Link>
+        </div>
+        <div className="w-full grid sm:grid-cols-1 md:grid-cols-2 gap-5">
+          {/* PROJECT */}
+          <div className="w-full border rounded-md p-3 min-h-24"></div>
+          <div className="w-full border rounded-md p-3 min-h-24"></div>
+          <div className="w-full border rounded-md p-3 min-h-24"></div>
+          <div className="w-full border rounded-md p-3 min-h-24"></div>
         </div>
       </div>
     </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Main from "./main";
-import Footer from "./footer";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
-    <div className="w-full sm:w-[90%] md:w-[85] lg:w-[60%] font-[family-name:var(--font-geist-sans)]">
+    <div className="w-full sm:w-[90%] md:w-[85] lg:w-[60%] p-5 font-[family-name:var(--font-geist-sans)]">
       <Main />
 
       <Footer />

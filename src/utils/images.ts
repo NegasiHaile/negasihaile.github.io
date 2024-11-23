736bd7ee032af451771d72c035a545d07654f674
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-11-23 23:02:47 +0300

    Added: Testmonials images import

---
+++ src/utils/images.ts
@@ -0,0 +1,7 @@
+import Testmonial_0 from "../assests/testmonials/Upwork-00.png";
+import Testmonial_1 from "../assests/testmonials/Upwork-01.png";
+import Testmonial_2 from "../assests/testmonials/Upwork-02.png";
+import Testmonial_3 from "../assests/testmonials/Upwork-03.png";
+import Testmonial_4 from "../assests/testmonials/Upwork-04.png";
+
+export { Testmonial_0, Testmonial_1, Testmonial_2, Testmonial_3, Testmonial_4 };

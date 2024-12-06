27ebabe360fdb8ca6f2e63a7e76377a443fc1eb2
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-12-06 16:16:55 +0300

    Added: Posts list

---
+++ src/data/posts.tsx
@@ -0,0 +1,163 @@
+import { AGP_1, Testmonial_0 } from "@/utils/images";
+
+export const posts = [
+  {
+    id: 3,
+    date: "Dec 01, 2024",
+    category: "general",
+    display: true,
+    title: `How to create a startup: 15 steps to succeed in the digital world`,
+    thumbnail:
+      "https://syndicode.com/wp-content/uploads/2024/05/USP-1-1024x576.png",
+    sub_title: `A startup is a new business that focuses on creating unique and innovative technology solutions or services. Known for their high growth potential, startups offer numerous benefits to the economy.
+
+This is why many countries actively encourage the creation of startups through financial incentives and simplified regulations.
+
+In fact, in some countries, small and medium-sized businesses make up a major part of the economy.
+
+So, if you are eager to solve a problem you are passionate about while enjoying autonomy and the potential for high returns, this guide is for you.`,
+    sections: [
+      {
+        id: 0,
+        title: <></>,
+        description: (
+          <>
+            You’re in the home stretch of your release cycle. It’s time to ship
+            your product...and you have to write release notes.
+            <br />
+            <br />
+            By the time many SaaS product teams reach the end of a release
+            cycle, they treat the release notes as an afterthought. They rush to
+            put something—anything—together to check the “release notes” box of
+            that particular development cycle.
+            <br />
+            <br />
+            The release notes either end up being full of technical jargon users
+            will not understand (and tune out), or they don’t clearly articulate
+            the value of the updates, fixes, and new features being made. That’s
+            a big mistake because…
+          </>
+        ),
+      },
+
+      {
+        id: 2,
+        title: <>Release notes are an opportunity.</>,
+        description: (
+          <>
+            They’re an opportunity to keep your customers excited about their
+            investment in your product. They can drive adoption of new features
+            and functionality, reduce concerns about bugs or known issues, and
+            even be a powerful marketing tool.
+            <br />
+            <br />
+            In this article, we’ll look at what release notes are, who should
+            write them, and how you and your users benefit from good release
+            notes. And don’t worry, we’ll be diving into real examples and a
+            template you can steal for your own release notes.
+          </>
+        ),
+      },
+    ],
+  },
+  {
+    id: 0,
+    date: "Dec 01, 2024",
+    category: "freelance",
+    display: true,
+    title: "Is Upwork payment protection REAL or FAKE?",
+    thumbnail: Testmonial_0,
+    sub_title: `In this post I will share my 3 years of freelancing experiance in Upwork, but mainly I will tell you my story about the: I will tell you every thing that you need to know about Upwork.`,
+    sections: [
+      {
+        id: 0,
+        title: <></>,
+        description: (
+          <>
+            In this post I will share my 3 years of freelancing experiance in
+            Upwork, but mainly I will tell you my story about the: I will tell
+            you every thing that you need to know about Upwork.
+          </>
+        ),
+        list: [
+          "Upwork payment protection reality",
+          "what are the drawbacks as a freelancer in upwork?",
+          "What you should do as a freelancer in Upwork?",
+        ],
+        images: [AGP_1],
+      },
+
+      {
+        id: 2,
+        title: <></>,
+        description: (
+          <>
+            In this post I will share my 3 years of freelancing experiance in
+            Upwork, but mainly I will tell you my story about the: I will tell
+            you every thing that you need to know about Upwork.
+          </>
+        ),
+        list: [
+          "Upwork payment protection reality",
+          "what are the drawbacks as a freelancer in upwork?",
+          "What you should do as a freelancer in Upwork?",
+        ],
+        images: [AGP_1],
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    date: "Dec 01, 2024",
+    category: "general",
+    display: true,
+    title: `How to write release notes (template +5 great examples)`,
+    thumbnail:
+      "https://cdn.prod.website-files.com/66c750efb3d2379780efac0c/6733599c88ece7f1d902e89f_654028c47386c68af981c2a2_Blog_Header_How-to-write-release-notes.png",
+    sub_title: `Release notes are a crucial aspect of software development, providing users with information about new or updated features, fixes, and changes. A well-written release note can enhance the user experience, improve the product's reputation, and reduce support requests. The excerpt highlights the importance of release notes and provides best practices for writing them. Don't treat your product release notes as an afterthought. Here are 5 examples that pack a punch—plus best practices for writing your own.`,
+    sections: [
+      {
+        id: 0,
+        title: <></>,
+        description: (
+          <>
+            You’re in the home stretch of your release cycle. It’s time to ship
+            your product...and you have to write release notes.
+            <br />
+            <br />
+            By the time many SaaS product teams reach the end of a release
+            cycle, they treat the release notes as an afterthought. They rush to
+            put something—anything—together to check the “release notes” box of
+            that particular development cycle.
+            <br />
+            <br />
+            The release notes either end up being full of technical jargon users
+            will not understand (and tune out), or they don’t clearly articulate
+            the value of the updates, fixes, and new features being made. That’s
+            a big mistake because…
+          </>
+        ),
+      },
+
+      {
+        id: 2,
+        title: <>Release notes are an opportunity.</>,
+        description: (
+          <>
+            They’re an opportunity to keep your customers excited about their
+            investment in your product. They can drive adoption of new features
+            and functionality, reduce concerns about bugs or known issues, and
+            even be a powerful marketing tool.
+            <br />
+            <br />
+            In this article, we’ll look at what release notes are, who should
+            write them, and how you and your users benefit from good release
+            notes. And don’t worry, we’ll be diving into real examples and a
+            template you can steal for your own release notes.
+          </>
+        ),
+      },
+    ],
+  },
+];

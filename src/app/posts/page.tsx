2811b36adf23c8d2d8034c47c2e0515854bf0684
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-12-06 16:22:13 +0300

    Added: Posts mapping function

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,20 @@
+import PostCard from "@/components/post-card";
+import { posts } from "@/data/posts";
 import React from "react";
 
 const Posts = () => {
   return (
     <div className="w-full">
-      <p className="text-xl "> No results found!</p>
+      {posts.length > 0 ? (
+        <div className="space-y-10">
+          {posts.map((post, i) => {
+            if (post.display === false) return null;
+            return <PostCard index={i} post={post} />;
+          })}
+        </div>
+      ) : (
+        <p className="text-xl "> No results found!</p>
+      )}
     </div>
   );
 };

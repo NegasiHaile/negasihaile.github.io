c1c9a98e6a5a55484cd68e5c66571355b34fcda5
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-12-07 17:54:02 +0300

    Updated: Postcard image vitals

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,4 +1,5 @@
 import { PostTypes } from "@/types";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -20,22 +21,23 @@ const PostCard = ({ index, post }: PostCardProps) => {
         <p className="opacity-50">{post.date}</p>
       </div>
 
-      <div className="block justify-between w-full space-y-3">
+      <div className="block lg:flex justify-between w-full space-y-3 lg:space-y-0 lg:space-x-5">
         <p className=" text-justify">{post.sub_title}</p>
         {post.thumbnail && (
-          // <Image
-          //   src={post.thumbnail}
-          //   alt={post.title}
-          //   width={400}
-          //   height={120}
-          // />
+          <Image
+            src={post.thumbnail}
+            alt={post.title}
+            width={350}
+            height={120}
+            className="w-fit lg:max-w-96 max-h-56 rounded"
+          />
 
-          <div
-            className=" w-full h-48  md:flex justify-center items-center rounded bg-cover bg-center"
-            style={{
-              backgroundImage: `url(${post.thumbnail})`,
-            }}
-          ></div>
+          // <div
+          //   className=" w-full h-full min-h-56 md:h-48  md:flex justify-center items-center rounded bg-cover bg-center"
+          //   style={{
+          //     backgroundImage: `url(${post.thumbnail})`,
+          //   }}
+          // ></div>
         )}
       </div>
 

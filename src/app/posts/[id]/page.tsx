58b2269fba7ac2709a47d722d4543f9d517eeeef
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-12-09 18:29:57 +0300

    Added: Post sections elements on post detail page

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -8,7 +8,7 @@ const Post = async ({ params }: { params: { id: number } }) => {
   return (
     <div className="space-y-5">
       <div className="justify-between space-y-5">
-        <p className="text-3xl md:text-5xl font-bold w-full">{post.title}</p>
+        <p className="text-4xl md:text-5xl font-bold w-full">{post.title}</p>
 
         <div
           className=" w-full h-fit min-h-96 justify-center items-center rounded bg-cover bg-center"
@@ -16,6 +16,33 @@ const Post = async ({ params }: { params: { id: number } }) => {
             backgroundImage: `url(${post.thumbnail})`,
           }}
         ></div>
+
+        <div className="space-y-14">
+          {post.sections.map((section, sec_i) => {
+            return (
+              <div key={sec_i} className="space-y-5">
+                <p className=" text-2xl md:text-3xl font-bold">
+                  {section.title}
+                </p>
+                <div className="text-xl leading-8 text-justify">
+                  {section.description}
+                </div>
+                {section.images.length > 0 &&
+                  section.images.map((image, image_i) => {
+                    return (
+                      <div
+                        key={`section_${sec_i}_image_${image_i}`}
+                        className=" w-full h-fit min-h-96 justify-center items-center rounded bg-cover bg-center"
+                        style={{
+                          backgroundImage: `url(${image})`,
+                        }}
+                      ></div>
+                    );
+                  })}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );

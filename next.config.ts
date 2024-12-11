38da1b294b21d5ae2f076e99eb422e9fd2d22781
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-12-11 16:03:46 +0300

    Added: Markdown rendering with next js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,22 @@
-import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+import { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
   output: "standalone",
   basePath: "/negasihaile",
   /* config options here */
   images: {
     unoptimized: true, // Disable image optimization (optional)
   },
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);

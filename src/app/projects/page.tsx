ef2c054d376e24ab71d3a6fedcaa16779ff70ab6
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2025-03-14 01:09:32 +0300

    Updated: projects page description

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,12 +5,12 @@ const Projects = () => {
   return (
     <div className="w-full space-y-5">
       <p className="opacity-80">
-        This list includes a catalog of all the projects I have worked and
-        completed and some inprogress. It covers a broad spectrum, from
-        professional freelancing work to personal projects that reflect my
-        individual creativity, skill-building, and passion. These projects can
-        include tasks from various domains like Web, Desktop, and Mobile
-        development and other specialized services like building
+        This list includes a catalog of all the projects I have completed and
+        some inprogress. It covers a broad spectrum, from professional
+        freelancing work to personal projects that reflect my individual problem
+        solving skills, career-building, and passion. These projects can include
+        tasks from various domains like Web development, data processing and
+        visalizatiom and other specialized services like building
         <b> business automation AI solutions</b>.
       </p>
       <ProjectsList display="all" />

2328066202e54f41e11ca58be95be3fc1a60f8c4
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2025-03-31 23:50:19 +0300

    Deploy

---
+++ _next/static/chunks/app/negasihaile/page-a09e82f1bbda5142.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[735,974],{265:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.bind(n,1300)),Promise.resolve().then(n.bind(n,5999))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,565,999,441,517,358],()=>s(265)),_N_E=e.O()}]);
